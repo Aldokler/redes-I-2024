97532a237eea4449f10dfd2d47b78082ef67869f
BritM <55718279+BritM@users.noreply.github.com>
2024-05-09 17:19:30 -0600

    Update Emisor.ino
    Estaba en little endian lo pasé a big endian

--- Proyecto1/Emisor/Emisor.ino
+++ Proyecto1/Emisor/Emisor.ino
@@ -26,8 +26,8 @@ void loop() {
     
     short tmp = (Serial.read() << 8) | (Serial.read());
 
-    data[8] = (byte)(tmp & 0xff);
-    data[9] = (byte)((tmp >> 8) & 0xff);
+    data[8] = (byte)((tmp >> 8) & 0xff);
+    data[9] = (byte)(tmp & 0xff );
 
     Serial.readBytes(data + 10, tmp);
 
