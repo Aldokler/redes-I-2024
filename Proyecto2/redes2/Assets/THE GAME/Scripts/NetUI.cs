a2b3dde48d61a0ba8ab28d5238508038b30b6c8b
Aldokler <xdavidpastor@gmail.com>
2024-06-10 12:05:19 -0600

    lan testing

--- Proyecto2/redes2/Assets/THE GAME/Scripts/NetUI.cs
+++ Proyecto2/redes2/Assets/THE GAME/Scripts/NetUI.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using Unity.Netcode;
 using UnityEngine;
+using Unity.Netcode.Transports.UTP;
+using TMPro;
+using System.Net;
+using System.Net.Sockets;
 
 public class NetworkManagerUI : MonoBehaviour
 {
     [SerializeField] private Button Btn_Server;
     [SerializeField] private Button Btn_Host;
     [SerializeField] private Button Btn_Client;
+
+    [SerializeField] TextMeshProUGUI ipAddressText;
+    [SerializeField] TMP_InputField ip;
+
+    [SerializeField] string ipAddress;
+    [SerializeField] UnityTransport transport;
 
     private void Awake()
     {
@@ -20,11 +30,41 @@ public class NetworkManagerUI : MonoBehaviour
         //Lamda expression / Delegate
         Btn_Host.onClick.AddListener(() => {
             NetworkManager.Singleton.StartHost();
+            GetLocalIPAddress();
         });
 
         //Lamda expression / Delegate
         Btn_Client.onClick.AddListener(() => {
+            ipAddress = ip.text;
+            SetIpAddress();
             NetworkManager.Singleton.StartClient();
         });
+    }
+
+    void Start()
+    {
+        ipAddress = "0.0.0.0";
+        SetIpAddress(); // Set the Ip to the above address
+    }
+
+    public string GetLocalIPAddress()
+    {
+        var host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (var ip in host.AddressList)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                ipAddressText.text = ip.ToString();
+                ipAddress = ip.ToString();
+                return ip.ToString();
+            }
+        }
+        throw new System.Exception("No network adapters with an IPv4 address in the system!");
+    }
+
+    public void SetIpAddress()
+    {
+        transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+        transport.ConnectionData.Address = ipAddress;
     }
 }
